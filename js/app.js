4b1ea15b55c02135ff5af18c9b932c58c0d6d503
Fareed <95282461+fhareed1@users.noreply.github.com>
2023-07-24 09:57:57 +0100

    Update app.js

--- js/app.js
+++ js/app.js
@@ -28,7 +28,7 @@ const preloader = document.querySelector(".preloader");
 
 setTimeout(function () {
   preloader.classList.add("hide-preloader");
-}, 6000);
+}, 5000);
 
 
 let mainParagraph = document.querySelector('.main-page-paragraph');
